ee33183019173271b3084abe3fb4e6018a0c3a89
Miguel García Da Silva <mikemgds@gmail.com>
2022-02-04 10:46:41 +0100

    * Plain Spring Boot project. First commit.

---
+++ src/main/java/es/miguelgsi/springboottestingprimer/SpringBootTestingPrimerApplication.java
@@ -0,0 +1,13 @@
+package es.miguelgsi.springboottestingprimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingPrimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestingPrimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/miguelgsi/springboottestingprimer/SpringBootTestingPrimerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.miguelgsi.springboottestingprimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingPrimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
