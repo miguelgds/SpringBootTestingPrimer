be6d4be22e1db7f1ddfaca8883651f25104e6b97
Miguel García Da Silva <mikemgds@gmail.com>
2022-02-09 13:34:09 +0100

    * Tests from exercise 7: ShoppingCartController purchaseArticle fail cases refactoring

--- src/test/java/es/miguelgsi/springboottestingprimer/ShoppingCartControllerTestIT.java
+++ src/test/java/es/miguelgsi/springboottestingprimer/ShoppingCartControllerTestIT.java
@@ -1,6 +1,5 @@
 package es.miguelgsi.springboottestingprimer;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -9,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
@@ -20,6 +18,8 @@ import java.util.UUID;
 
 import static es.miguelgsi.springboottestingprimer.Currency.DOLLAR;
 import static es.miguelgsi.springboottestingprimer.Currency.EURO;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -97,7 +97,7 @@ class ShoppingCartControllerTestIT {
                         .content("{\"username\": \"username\", \"currency\": \"DOLLAR\"}")
                         .with(csrf()))
                     .andExpect(status().isCreated())
-                    .andExpect(header().string("location", "http://localhost/v1/shopping-cart/1/sales/" + saleId.getId().toString()));
+                    .andExpect(header().string("location", containsString("/v1/shopping-cart/1/sales/" + saleId.getId().toString())));
 
             verify(shoppingCartUseCase).purchase(purchase);
         }
@@ -109,8 +109,10 @@ class ShoppingCartControllerTestIT {
                             .content("{\"username\": \"usr\", \"currency\": \"DOLLAR\"}")
                             .with(csrf()))
                     .andExpect(status().is4xxClientError())
-                    .andExpect(result -> Assertions.assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException))
-                    .andExpect(result -> Assertions.assertTrue(result.getResolvedException().getMessage().contains("Username field must have a size between 5 and 50")));
+                    .andExpect(result -> assertThat(result.getResolvedException())
+                            .isInstanceOf(MethodArgumentNotValidException.class))
+                    .andExpect(result -> assertThat(result.getResolvedException().getMessage())
+                            .contains("Username field must have a size between 5 and 50"));
         }
 
         @Test
@@ -120,7 +122,8 @@ class ShoppingCartControllerTestIT {
                             .content("{\"username\": \"username\", \"currency\": \"PESETA\"}")
                             .with(csrf()))
                     .andExpect(status().is4xxClientError())
-                    .andExpect(result -> Assertions.assertTrue(result.getResolvedException().getMessage().contains("\"PESETA\": not one of the values accepted for Enum class")));
+                    .andExpect(result -> assertThat(result.getResolvedException().getMessage())
+                            .contains("\"PESETA\": not one of the values accepted for Enum class"));
         }
 
         @Test
@@ -130,7 +133,8 @@ class ShoppingCartControllerTestIT {
                             .content("{\"currency\": \"DOLLAR\"}")
                             .with(csrf()))
                     .andExpect(status().is4xxClientError())
-                    .andExpect(result -> Assertions.assertTrue(result.getResolvedException().getMessage().contains("Username field is mandatory")));
+                    .andExpect(result -> assertThat(result.getResolvedException().getMessage())
+                            .contains("Username field is mandatory"));
         }
 
         @Test
@@ -140,7 +144,8 @@ class ShoppingCartControllerTestIT {
                             .content("{\"username\": \"username\"}")
                             .with(csrf()))
                     .andExpect(status().is4xxClientError())
-                    .andExpect(result -> Assertions.assertTrue(result.getResolvedException().getMessage().contains("Currency field is mandatory")));
+                    .andExpect(result -> assertThat(result.getResolvedException().getMessage())
+                            .contains("Currency field is mandatory"));
         }
     }
 }
